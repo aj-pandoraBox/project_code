097ec652749087e2c555b401f63174f509d6b5d8
aj-pandoraBox <37234350+aj-pandoraBox@users.noreply.github.com>
2020-10-12 12:15:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"  >
+    <title>Document</title>
+    <link rel="stylesheet" href="ven/css/normalize.css">
+    <link rel="stylesheet" href="ven/css/grid.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="resources/css/style.css">
+    <link rel="stylesheet" href="resources/css/queries.css">
+    
+</head>
+<body>
+    <header>
+       
+       <div class="logo-header">
+           <h3><ion-icon name="book-sharp"></ion-icon></h3>
+           
+       </div>
+       
+       <nav>
+           <ul>
+               <li>Online Courses</li>
+               <li>About Us</li>
+               <li>Contact</li>
+               <li>NavBar</li>
+               
+           </ul>
+           
+       </nav>
+       
+       <div class="header-welcome-container">
+        <h2 class="tag-line-header">Grow up Your <br>Communication<br> skills.</h2>
+        <p>Learn from the world’s best teachers in the comfort of home. LIVE, Interactive, Fun.</p>
+        <button class="btn-search-courses-header">Start Browsing Courses</button>
+        
+        </div>        
+        
+        
+    </header>
+    
+    
+    
+    <section class="info-header-2-section">
+        
+        
+        <div class="info-card-header">
+            <h3><ion-icon name="chatbox-sharp"></ion-icon></h3>
+            <h4>Live Chating</h4>
+            <p>Free Chat Rooms Online with no registration, you can enter and exit whenver u want to.</p>
+        </div>
+        
+         <div class="info-card-header">
+            <h3><ion-icon name="help-circle-sharp"></ion-icon></h3>
+            <h4>Questions</h4>
+            <p>Free Chat Rooms Online with no registration, you can enter and exit whenver u want to.</p>
+        </div>
+         <div class="info-card-header">
+            <h3><ion-icon name="code-slash-sharp"></ion-icon></h3>
+            <h4>Code</h4>
+            <p>Free Chat Rooms Online with no registration, you can enter and exit whenver u want to.</p>
+        </div>
+        
+        
+    </section>
+    
+    
+    
+    
+</body>
+</html>
+<script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
